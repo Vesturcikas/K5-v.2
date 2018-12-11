41b515e97ca0cb0bfbd700b369578c84ccdb1a20
Vesturcikas <vestas1976@gmail.com>
2018-12-12 00:32:38 +0200

    Add project files.

---
+++ K5-v.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K5_v._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
